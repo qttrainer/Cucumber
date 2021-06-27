26a4ae7ea85f06d996b3392c48bcb291c1522327
sqttrainer <yoursqttrainer@gmail.com>
2021-06-27 10:59:58 +0530

    First commit

---
+++ src/javaBasic/Address.java
@@ -0,0 +1,18 @@
+package javaBasic;
+
+public class Address {
+	
+	
+	int StreetNum;
+	String city;
+	String country;
+	public Address(int streetNum, String city, String country) {
+		StreetNum = streetNum;
+		this.city = city;
+		this.country = country;
+	}
+	
+	
+	
+
+}
