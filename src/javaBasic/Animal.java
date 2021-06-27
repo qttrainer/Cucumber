6c16753d37f6f0943a0a223fc59e0b49c2a5a9ba
sqttrainer <yoursqttrainer@gmail.com>
2021-06-27 11:05:35 +0530

    addded animal class

---
+++ src/javaBasic/Animal.java
@@ -0,0 +1,20 @@
+package javaBasic;
+
+public   class Animal {
+	
+	final String name = "Animal";
+	
+	
+	public  void displayInfo()
+	{
+		System.out.println("I am a animal");
+		
+		
+	}
+	
+	public void setName(String name)
+	{
+		//this.name = name;
+	}
+
+}
